e0dc19c9cde371768f23817e76f80ee76e1c1a08
HakemHa <65263592+HakemHa@users.noreply.github.com>
2024-08-25 01:10:36 +0300

    Delete src directory
    Reset

--- src/store/store.jsx
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import markReducer from "./slice";
-
-const store = configureStore({ reducer: { markdown: markReducer } });
-
-export default store;
